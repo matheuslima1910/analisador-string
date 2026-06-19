b0bb796c8cdcf354c6fc4e6e85d1a7ef524e5844
matheuslima1910 <matheuslrs1999@gmail.com>
2026-06-18 21:30:07 -0300

    Analisador de String

---
+++ AnalisadorString.py
@@ -0,0 +1,12 @@
+# Solicitando ao usuário a entrada da informação
+a = input("Digite algo: ").strip()
+
+# Analisando o dado informado
+print("O tipo primitivo desse valor é ", type(a))
+print("Só tem espaços? ", a.isspace())
+print("É um número? ", a.isnumeric())
+print("É alfabético? ", a.isalpha())
+print("É alfanumérico? ", a.isalnum())
+print("Está em maiúsculas? ", a.isupper())
+print("Está em minúsculas? ", a.islower())
+print("Está capitalizada? ", a.istitle())
